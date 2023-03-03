3dfcaa6ec01bc141ed2d3bd26c842d5c44d08651
Wendrick Cardoso <wendrick.estudos@gmail.com>
2023-03-02 23:50:41 -0300

    Testando variáveis e tipos

--- java-1-basics/java-1-syntaxJava/java-2-TypesAndVars/src/typesAndVars.java
+++ java-1-basics/java-1-syntaxJava/java-2-TypesAndVars/src/typesAndVars.java
@@ -8,7 +8,10 @@ public class typesAndVars {
         int x2 = 2;  /// Quantidade Normal          PADRÃO
         long x3 = 3; /// Quantidade Grande
         
-
+        String f = "Textos";     /// Textos e frases
+        boolean truthFake = true;    /// Verdadeiro ou falso
+        boolean fakeTruth = false;
+        char c = 'D';    /// Apenas uma letra
 
         float z = 2.5f;     /// Quantidade Normal   ( PRECISA BOTAR O F NO FINAL OU (float) antes do número)
         double z1 = 45.45;  /// Quantidade Grande   PADRÃO
@@ -16,5 +19,9 @@ public class typesAndVars {
         final int v = 1;   /// Nunca muda a variável
 
         System.out.print(x + x1 + x2 + x3 + z + z1 + v);
+        System.out.print("\n" + f);
+        System.out.print("\n" + c);
+        System.out.print("\n" + truthFake);
+        System.out.print("\n" + fakeTruth);
     }
 }
