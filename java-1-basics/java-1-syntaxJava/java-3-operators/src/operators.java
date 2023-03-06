b3a4173de3736042b0ddd42a625d54f28bc3cf81
Wendrick Cardoso <wendrick.estudos@gmail.com>
2023-03-06 00:17:45 -0300

    Terminando Estudos operadores

--- java-1-basics/java-1-syntaxJava/java-3-operators/src/operators.java
+++ java-1-basics/java-1-syntaxJava/java-3-operators/src/operators.java
@@ -1,6 +1,8 @@
 public class operators {
     public static void main(String[] args) {
-        int valor = 5;  /// =  é o operador de ATRIBUIÇÃO
+        /// =  é o operador de ATRIBUIÇÃO
+        int valor = 5; 
+
         System.out.print("O Valor é: " + (valor));
 
         ///    +    ===  Soma
@@ -16,7 +18,44 @@ public class operators {
         System.out.print("\nDivisão: " + (valor / 5));
 
         ///    + ou -   ===  Manipulador de valor
-        System.out.print("\nValor negativo: " + (- valor));
+        System.out.print("\n\nValor negativo: " + (- valor));
         System.out.print("\nValor positivo: " + (+ valor));
+
+        ///    ++ ou --   ====  Incremento ou descremento
+            ///Printar na ordem   se for  ++valor incrementa depois printa
+            /// Se for valor++ printa depois incrementa
+        System.out.print("\n\nValor Incrementado: " + (++valor));
+        System.out.print("\nValor Descrementado: " + (--valor));
+
+        ///    !  === Negar
+        boolean valores = true;
+
+        System.out.print("\n\nPrintar BOOLEAN: " + valores);
+        System.out.print("\nValor Negado: " + (!valores));
+
+        /// ? === coondicional
+        System.out.print("\n\nValor TERNÁRIO: " + (valores==true ? "verdadeiro" : "Falso"));
+        System.out.print("\nValor TERNÁRIO: " + (!valores==true ? "verdadeiro" : "Falso"));
+
+        ///   ==    :   Igual
+        System.out.print("\n\nValor IGUAL: " + (valores==true ? "verdadeiro" : "Falso"));
+        ///   !=    :   Diferente
+        System.out.print("\nValor DIFERENTE: " + (valores!=true ? "verdadeiro" : "Falso"));
+
+        ///    >   :    Maior
+        System.out.print("\n\nValor Maior: " + (valor > --valor));
+        ///    <   :   Menor
+        System.out.print("\nValor Menor: " + (valor < ++valor));
+
+        boolean valores2 = true;
+        ///    Lógica E   TRUE
+        System.out.print("\n\nValor E: " + (valores && valores2));
+        ///    Lógica E   FALSE
+        System.out.print("\nValor E: " + (valores && !valores2));
+        ///    Lógica OU   TRUE
+        System.out.print("\nValor E: " + (valores || !valores2));
+        ///    Lógica OU   FALSE
+        System.out.print("\nValor E: " + (!valores || !valores2));
+        
     }
 }
